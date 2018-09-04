0151bc8caafa1a35e926533599ac489ca881ded7
brianngeno <bkn.ngeno@gmail.com>
2018-09-04 16:05:27 +0300

    intial commit with data base

--- app/main/views.py
+++ app/main/views.py
@@ -3,6 +3,7 @@ from . import main
 from ..request import get_movies,get_movie,search_movie
 from .forms import ReviewForm
 from ..models import Review
+from flask_login import login_required
 
 # Review = review.Review
 
@@ -26,7 +27,7 @@ def index():
     search_movie = request.args.get('movie_query')
 
     if search_movie:
-        return redirect(url_for('search', movie_name = search_movie))
+        return redirect(url_for('.search', movie_name = search_movie))
     else:    
         return render_template('index.html', title = title, popular = popular_movies, upcoming = upcoming_movies, now_showing = now_showing_movie)
 
@@ -54,6 +55,7 @@ def search(movie_name):
     return render_template('search.html',movies = searched_movies)    
 
 @main.route('/movie/review/new/<int:id>', methods = ['GET','POST'])
+@login_required
 def new_review(id):
     form = ReviewForm()
     movie = get_movie(id)
--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 export MOVIE_API_KEY='1da453f27393ac3b85881f5f866c58fd'
-export SECRET_KEY='lamba lolo'
+export SECRET_KEY='movie'
 
 python3.6 manage.py server
